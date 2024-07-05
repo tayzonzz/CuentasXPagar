aaf19eeaba0961738a0d4c1807616bffbe5922e4
Tay <c.robles1@unapec.edu.do>
2024-07-05 18:46:48 -0400

    1st Commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CuentasXPagar.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CuentasXPagar.Startup))]
+namespace CuentasXPagar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
